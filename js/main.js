80d849c4550f39b3b5626f542fc19b94e37c1dbf
mr-dargi <dargimohammadreza@gmail.com>
2023-02-04 16:14:51 +0330

    change some mistakes about time add reset function and changes timer display inside the html page

--- js/main.js
+++ js/main.js
@@ -1,50 +1,60 @@
-let 
-hours = 0, 
-minutes = 0, 
-seconds = 0, 
-miliseconds = 0;
+let [hours, minutes, seconds, miliseconds] = [0, 0, 0, 0];
 
+let start = document.getElementById("startTimer");
+let stop = document.getElementById("stopTimer");
+let reset = document.getElementById("resetTimer");
 let int = null;
 
-const timer = document.getElementById("timer");
-const start = document.getElementById("startTimer");
-const stop = document.getElementById("stopTimer");
-
+let timer = document.getElementById("timer");
 
 const startTimer = () => {
-    miliseconds++;
+  miliseconds+= 10;
 
-    if(miliseconds == 999) {
-        seconds++;
-        miliseconds = 0;
+  if(miliseconds == 1000) {
+    seconds++;
+    miliseconds = 0;
 
-        if(seconds == 59) {
-            minutes++;
-            seconds = 0;
+    if(seconds == 60) {
+      minutes++;
+      seconds = 0;
 
-            if(minutes == 59) {
-                hours++;
-                minutes = 0;
-            }
+      if(minutes == 60){
+        hours++;
+        minutes = 0;
+
+        if(hours == 60) {
+          clearInterval(int);
         }
+      }
     }
+  }
 
-    timer.innerHTML = `${hours}:${minutes}:${seconds}:${miliseconds}`
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms = miliseconds < 10 ? "00" + miliseconds : miliseconds < 100 ? "0" + miliseconds : miliseconds;
+
+  timer.innerHTML = `${h}:${m}:${s}:${ms}`;
 }
 
 const myInterval = () => {
-    if(int !== null) {
-        debugger
-        clearInterval(int);
-    }
+  if(int !== null) {
+    clearInterval(int);
+  }
 
-    int = setInterval(startTimer, 1);
-    console.log(int);
+  int = setInterval(startTimer, 10);
 }
 
 start.addEventListener("click", myInterval);
+
 stop.addEventListener("click", () => {
-    debugger
-    clearInterval(int);
-    console.log(int);
+  clearInterval(int);
 })
+
+reset.addEventListener("click", () => {
+  clearInterval(int);
+
+  [hours, minutes, seconds, miliseconds] = [0, 0, 0, 0];
+  timer.innerHTML = "00:00:00:000";
+})
+
