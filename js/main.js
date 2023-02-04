00c9802e585f45b5f01c69a956891beb646a9cb7
mr-dargi <dargimohammadreza@gmail.com>
2023-02-04 13:08:07 +0330

    set a start and stop button functions in JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-let [hour, minute, second, milisecond] = 0;
+let [hours, minutes, seconds, miliseconds] = 0;
+
+
+const startTimer = () => {
+    miliseconds++;
+
+    if(miliseconds == 999) {
+        seconds++;
+        miliseconds = 0;
+
+        if(seconds == 59) {
+            minutes++;
+            seconds = 0;
+
+            if(minutes == 59) {
+                hours++;
+                minutes = 0;
+            }
+        }
+    }
+}
+
+const timerInterval = setInterval(startTimer(),1);
+
+document.getElementById("startTimer").addEventListener("click", timerInterval());
+document.getElementById("stopTimer").addEventListener("click", () => {
+    clearInterval()
+})
